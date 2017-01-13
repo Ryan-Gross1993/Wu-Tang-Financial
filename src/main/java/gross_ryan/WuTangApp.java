dcaf587c967793ec9cc37b0a33dc01bcfdc42356
Ryan Gross <rgross@udel.edu>
2017-01-13 17:19:55 -0500

    Added WuTangApp class

---
+++ src/main/java/gross_ryan/WuTangApp.java
@@ -0,0 +1,12 @@
+package gross_ryan;
+
+/**
+ * Created by ryangross on 1/13/17.
+ */
+public class WuTangApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hey");
+    }
+    
+}
